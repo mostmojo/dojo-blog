b6b564d207003d12bf1ee0052f07217485bcead5
Jacob <jacob.papageorgiou@gmail.com>
2021-06-26 16:19:54 +0300

    Add dynamic value tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the blog!"
+  const likes = 50;
+  const link = "https://google.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <a href={ link }>View</a>
       </div>
     </div>
   );
