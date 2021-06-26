afd873ab603184efc1dd51eade9869064a01bb54
Jacob <jacob.papageorgiou@gmail.com>
2021-06-26 16:27:27 +0300

    Add home componenet with Nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the blog!"
-  const likes = 50;
-  const link = "https://google.com"
+  // const title = "Welcome to the blog!"
+  // const likes = 50;
+  // const link = "https://google.com"
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
+        {/* <h1>{ title }</h1>
         <p>Liked { likes } times</p>
-        <a href={ link }>View</a>
+        <a href={ link }>View</a> */}
+        <Home />
       </div>
     </div>
   );
